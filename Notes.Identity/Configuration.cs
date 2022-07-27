21a611a488ff9da9ead2aee8f2e427485d9c4741
Сергей Воробьев <kazan1003@gmail.com>
2022-07-27 17:07:53 +0300

    Добавьте файлы проекта.

---
+++ Notes.Identity/Configuration.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace Notes.Identity
+{
+    /// <summary>
+    /// содержит информацию о клиентах, ресурсах и т.д
+    /// </summary>
+    public static class Configuration
+    {
+        /// <summary>
+        /// Области, которые клиентскому приложению можно использовать(Идентификатор, который отправляется во время
+        /// аутентификации в процессе запроса токена
+        /// </summary>
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("NotesWebApi", "Web API")
+            };
+        /// <summary>
+        /// identityResource позволяет смоделировать область, которое позволит клиентскому приложению просматривать
+        /// множество утверждений о пользователе
+        /// </summary>
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        /// <summary>
+        /// Позволяет смоделировать доступ ко всему защищаемому ресурсу
+        /// </summary>
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("NotesWebAPI", "Web API", new []{JwtClaimTypes.Name})
+                {
+                    Scopes = { "NotesWebAPI" }
+                }
+            };
+
+        /// <summary>
+        /// список клиентских приложений, которые могут использовать наш api
+        /// </summary>
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "notes-web-api", //клиент id на клиенте должен быть = id клиента на сервере
+                    ClientName = "Notes Web",
+                    AllowedGrantTypes = GrantTypes.Code,  //используем autorization code
+                    RequireClientSecret = false,//не исп. secret клиента (обычно это sha256 строка)
+                    RequirePkce = true,         //нужен ключ подтверждения для autorization code
+                    RedirectUris =              //набор адресов, куда может происходить перенаправление после аутентификации
+                    {                           //клиентского приложения
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =        //набор uri адресов, кому позволено использовать identityserver
+                    {
+                         "http://..."
+                    },                          
+                    PostLogoutRedirectUris =    //набор uri адресов, на которые переадресовывает после выхода клиентского
+                    {                           //приложения
+                        "http://.../signout-oidc"
+                    },
+                    AllowedScopes =             //области (scopes) доступные клиенту
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "NotesWebApi"
+                    },
+                    AllowAccessTokensViaBrowser = true  //управляет передачей токена через браузер
+                }
+            };
+    }
+}
---
+++ Notes.Identity/Startup.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4;
+using Microsoft.Extensions.Configuration;
+
+namespace Notes.Identity
+{
+    public class Startup
+    {
+       
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration configuration) => AppConfiguration = configuration;
+        
+
+        /// <summary>
+        /// Добавление всех сервисов, которые планируется использовать в приложении
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(Configuration.ApiResources)
+                .AddInMemoryIdentityResources(Configuration.IdentityResources)
+                .AddInMemoryApiScopes(Configuration.ApiScopes)
+                .AddInMemoryClients(Configuration.Clients)
+                .AddDeveloperSigningCredential();
+        }
+
+        /// <summary>
+        /// Здесь настраивается конвейер обработки запроса. Применяются все middleware
+        /// выполняются в том порядке, в ктором добавляются в конвейер
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();  //используем identityserver
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("huy");
+                });
+            });
+
+        }
+    }
+}
